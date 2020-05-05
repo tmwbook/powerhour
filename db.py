eaaa68f67020f0fef72e80439ddb91b6c356eb64
Tom White <tmwbook@gmail.com>
2020-05-04 22:17:18 -0400

    Add PlaylistRequest model

--- db.py
+++ db.py
@@ -8,6 +8,18 @@ class User(UserMixin, db.Model):
     spotify_id = db.Column(db.String, nullable=False)
     access_token = db.Column(db.String, nullable=False)
     refresh_token = db.Column(db.String, nullable=False)
+    requests = db.relationship('PlaylistRequest', backref='user', lazy=True)
 
     def __repr__(self):
-        return f'<User {self.id}: {spotify_id}'
+        return f'<User {self.id}: {self.spotify_id}>'
+
+
+class PlaylistRequest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    playlist_id = db.Column(db.Text, nullable=False)
+    # This is server local time, logic will be done in timedeltas
+    time = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
+        return f'<PlaylistRequest {self.id}: {self.playlist_id} from {self.user}>'
